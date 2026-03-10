fc06e5e2dd43d5495327fc6e7f1ab00bbab418b3
sky <sky@storis.com>
2026-03-09 21:32:16 -0400

    Add React Router with placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+
 function App() {
   return (
     <div>
-      <h1>My Cash App</h1>
-      <p>Payment application - React + TypeScript + Vite</p>
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link to="/signin" style={{ marginRight: '1rem' }}>Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
+      </nav>
+
+      <div style={{ padding: '1rem' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
     </div>
   )
 }
